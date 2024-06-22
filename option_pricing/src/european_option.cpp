8931ec06b2ab4e33811451bb2e502076752cd7a4
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-06-22 14:25:49 -0500

    Update european_option.cpp

--- option_pricing/src/european_option.cpp
+++ option_pricing/src/european_option.cpp
@@ -1,5 +1,6 @@
 #include "european_option.h"
 #include <cmath>
+#include <iostream>
 
 EuropeanOption::EuropeanOption(double S0_, double K_, double r_, double volatility_, double T_, char type_)
     : Option(S0_, K_, r_, volatility_, T_), type(type_) {}
@@ -11,7 +12,7 @@ double EuropeanOption::price() const {
     double discounted_expected_payoff = 0.0;
 
     for (int i = 0; i < num_simulations; ++i) {
-        double S_T = S0 * exp((r - 0.5 * volatility * volatility) * T + volatility * sqrt(T) * random_normal(0.0, 1.0));
+        double S_T = MonteCarlo::geometric_brownian_motion(S0, r, volatility, T, dt);
         double payoff = (type == 'C') ? std::max(S_T - K, 0.0) : std::max(K - S_T, 0.0);
         discounted_expected_payoff += payoff / num_simulations;
     }
